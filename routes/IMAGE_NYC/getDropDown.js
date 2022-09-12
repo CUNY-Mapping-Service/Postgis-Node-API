f413abcf77676c4f5e29ec30988e5d6b2e1d8a07
CUNY-Mapping-Service <cunymapping@gc.cuny.edu>
2022-09-12 12:49:47 -0400

    image routes

---
+++ routes/IMAGE_NYC/getDropDown.js
@@ -0,0 +1,68 @@
+//IMAGE NYC ONLY
+// route query
+const qc = require("node-cache");
+const queryCache = new qc();
+
+const sql = (params, query) => {
+  return `SELECT * FROM category_dropdowns`
+}
+
+// route schema
+const schema = {
+  description: 'List tables and views. Note the service user needs read permission on the geometry_columns view.',
+  tags: ['IMAGE NYC'],
+  summary: 'get dropdown list'
+}
+
+// create route
+module.exports = function (fastify, opts, next) {
+  fastify.route({
+    method: 'GET',
+    url: '/get_dropdown',
+    schema: schema,
+    handler: function (request, reply) {
+      fastify.pg.connect(onConnect)
+
+      function onConnect(err, client, release) {
+        if (err) return reply.send({
+          "statusCode": 500,
+          "error": "Internal Server Error",
+          "message": "unable to connect to database server. This endpoint is for IMAGE NYC ONLY"
+        })
+        const key = request.url
+        const cachedResp = queryCache.get(key);
+        if (typeof cachedResp !== 'undefined') {
+          release();
+          reply.send(cachedResp);
+        } else {
+          client.query(
+            sql(request.params, request.query),
+            function onResult(err, result) {
+              release()
+             
+              let main = result.rows.filter(m => m.cattype === 'main').sort((a,b)=>+a.sortorder-+b.sortorder);
+              const sub1 = result.rows.filter(s => s.cattype === 'sub1').sort((a,b)=>+a.sortorder-+b.sortorder);
+              const sub2 = result.rows.filter(s => s.cattype === 'sub2').sort((a,b)=>+a.sortorder-+b.sortorder);
+
+              sub1.forEach((s1)=>{
+                s1.sub2List = sub2.filter(s => s.subgroup1 === s1.id)
+              })
+
+              main.forEach((mainGroup) => {
+                mainGroup.sub1List = sub1.filter(s => s.maingroup === mainGroup.id)
+              });
+
+              reply.send(err || main)
+              if (result && typeof result !== 'undefined' && result.rows) {
+                queryCache.set(key, main, 900)
+              }
+            }
+          )
+        }
+      }
+    }
+  })
+  next()
+}
+
+module.exports.autoPrefix = '/v1'

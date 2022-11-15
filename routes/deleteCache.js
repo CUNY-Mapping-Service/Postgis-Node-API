3b26a0b3f053949ced92112f8526577a54150385
Field <wfield@gc.cuny.edu>
2022-11-15 14:54:40 -0500

    api key

--- routes/deleteCache.js
+++ routes/deleteCache.js
@@ -4,41 +4,32 @@ const schema = {
     description: 'Delete Cache',
     tags: ['meta'],
     summary: 'delete cache tables',
-    querystring: {
-        filter: {
-            type: 'string',
-            description: 'Optional filter parameters for a SQL WHERE statement.'
-        }
-    }
+    security: [
+      {
+        "apiKey": []
+      }
+    ]
 }
 
 const url = `/delete-cache`;
 const os = process.env.OS || 'WIN';
 
-function isWhitelisted(_ip) {
-    const whitelist = process.env.CACHE_RM_ALLOWED_IP.split(",");
-    console.log(whitelist)
-    for (let i = 0; i < whitelist.length; i++) {
-        if (whitelist[i] === _ip) {
-            return true;
-        }
-    }
-    return false;
-}
 module.exports = function (fastify, opts, next) {
+   
+
     fastify.route({
         method: 'GET',
         url: url,
         schema: schema,
-        handler: function (request, reply) {
-            console.log(request.ip.split(':')[0])
-            if (!isWhitelisted(request.ip.split(':')[0])) {
+        preHandler: (request, reply, done) => {
+            if(request.headers['x-api-key']!==process.env.PASSWORD){
                 reply
                     .code(500)
-                    //.header('Content-Type', 'application/json; charset=utf-8')
-                    .send('Action not allowed from this IP')
-                return
+                    .send('Authorization required')
             }
+            done();
+        },
+        handler: function (request, reply) {
             try {
                 switch (os) {
                     case 'UNIX':
@@ -51,7 +42,6 @@ module.exports = function (fastify, opts, next) {
 
                 reply
                     .code(200)
-                    //.header('Content-Type', 'application/json; charset=utf-8')
                     .send('Cache Deleted!')
             } catch (e) {
                 reply

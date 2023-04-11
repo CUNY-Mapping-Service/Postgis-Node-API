82bf6331c6ead2737890424357741029c712620c
CUNY-Mapping-Service <cunymapping@gc.cuny.edu>
2023-04-11 15:15:30 -0400

    damn cache

--- routes/deleteCache.js
+++ routes/deleteCache.js
@@ -1,4 +1,4 @@
-//const cp = require("child_process");
+const cp = require("child_process");
 //const recache = require("recache")
 const cache = require('../cache');
 const schema = {
@@ -15,6 +15,10 @@ const schema = {
 const url = `/delete-cache`;
 const os = process.env.OS || 'WIN';
 const fs = require('fs')
+//const fsExtra = require('fs-extra');
+const fsAsync = require('node:fs/promises');
+const path = require('path')
+
 module.exports = function (fastify, opts, next) {
 
 
@@ -33,27 +37,30 @@ module.exports = function (fastify, opts, next) {
             }
             done();
         },
-        handler: function (request, reply) {
+        handler: async function (request, reply) {
             const cacheFolder = `${process.argv.slice(2)[0]}` || '.';
 
           //  console.log(cacheFolder)
 
-           const pathToEmpty = `${cacheFolder}/${process.env.CACHE_FOLDER || 'tilecache'}`;
+           const relPath = `${cacheFolder}/${process.env.CACHE_FOLDER || 'tilecache'}`;
+          const pathToEmpty = path.join(__dirname.replace('\\routes',''),path.normalize(relPath));
+
            cache.destroy();
-           fs.rmSync(pathToEmpty, { recursive: true, force: true });
-           //cache.stop();
-                             //require('fs-extra').emptyDir(pathToEmpty).then(()=>{
-                                  cache.start(()=>{console.log('started')});
-                                  reply
-                        .code(200)
-                        .send(cache.list())
-                           //  })
-                .catch(err => {
-                    reply
-                        .code(500)
-                        .header('Content-Type', 'application/json; charset=utf-8')
-                        .send(err)
-                })
+        
+          cp.exec(`del /q /s ${path.normalize(pathToEmpty)}\\*.mvt`)
+         // await fsAsync.unlink(path.normalize(pathToEmpty))
+           // await fsAsync.readdir(path.normalize(pathToEmpty)).then((f) => {console.log(f); Promise.all(f.map(e => fsAsync.unlink(e)))})
+               cache.start();
+
+            reply
+                .code(200)
+                .send('success')
+         //  })
+
+
+         
+                                 
+
 
         }
     })

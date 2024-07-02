6e530fc0dc91b15a58dddb7761e4e0391f486123
CUNY-Mapping-Service <cunymapping@gc.cuny.edu>
2024-07-02 14:56:31 -0400

    prepost cfb

---
+++ app_specific_routes/CFB/totalByGeoAndCode.js
@@ -0,0 +1,93 @@
+const qc = require("node-cache");
+const queryCache = new qc({ stdTTL: 600, checkperiod: 300 } );
+// route query
+const sql = (params, query) => {
+	console.log(params)
+	console.log(query)
+
+	const geoFilter =(query.geocode && query.geovalue)?`AND ${query.geocode}='${query.geovalue}'`:'';
+
+	const preOrPost = query.prepost === 'pre' ? '<=' : query.prepost === 'post' ? '>' : false;
+		const statementColName = params.table === 'vw_contributionsbyelectedoffice' ? 'statement_no_totals' : 'statement_no';
+
+	const prePostFilter = !preOrPost?'': `AND ${statementColName} ${preOrPost} ${query.cutoff}`;
+
+	let tableStatement = `SELECT ${query.geocode}, SUM(contribtotamt) AS total_sum, SUM(contribtotnum) AS total_num
+	FROM ${params.table}
+	where curcode='${query.curcode}' ${geoFilter} ${prePostFilter}
+	GROUP BY ${query.geocode};`
+
+	console.log(tableStatement)
+    return tableStatement
+}
+
+// route schema
+const schema = {
+  description: 'Query a table or view.',
+  tags: ['api'],
+  summary: 'table query',
+  params: {
+    table: {
+      type: 'string',
+      description: 'The name of the table or view.'
+    }
+  },
+  querystring: {
+    geocode: {
+      type: 'string',
+      description: "Geo code eg. curzip"
+    },
+    geovalue:{
+      type: 'string',
+      description: "Geo code eg. 10001"
+    },
+    curcode: {
+      type: 'string',
+      description: 'Curcode filter eg. AllAll'
+    }
+  }
+}
+
+// create route
+module.exports = function (fastify, opts, next) {
+  fastify.route({
+    method: 'GET',
+    url: '/sumByGeoAndCode/:table',
+    schema: schema,
+    handler: function (request, reply) {
+      fastify.pg.connect(onConnect)
+      console.log('connect')
+      function onConnect(err, client, release) {
+        if (err) return reply.send({
+          "statusCode": 500,
+          "error": "Internal Server Error",
+          "message": "unable to connect to database server"
+        })
+        
+        const key = request.url
+        const cachedResp = queryCache.get(key);
+        console.log('cached: ',typeof cachedResp !== 'undefined');
+        if (typeof cachedResp !== 'undefined') {
+          release();
+          reply.send(cachedResp);
+        } else {
+         console.log('not cached')
+          client.query(
+            sql(request.params, request.query),
+            function onResult(err, result) {
+              if(err) console.log(sql(request.params, request.query))
+              release()
+              reply.send(err || result.rows)
+              if (result && typeof result !== 'undefined' && result.rows){
+                queryCache.set(key, result.rows, 900)
+              }
+            }
+          )
+        }
+      }
+    }
+  })
+  next()
+}
+
+module.exports.autoPrefix = '/v1'
